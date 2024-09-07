e839aba9c4c90d13469cb92c6e6b41b74d53bb49
Pedro Gama <pedrogamase@gmail.com>
2024-09-07 06:32:28 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,50 @@
-
-document.querySelector('.busca').addEventListener('submit', async (event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event) => {
     event.preventDefault();
 
     let input = document.querySelector('#searchInput').value;
-    if(input !=='')
-    {
-        clearInfo();    
-        showWarning('Carregando...')
-        
+    if (input !== '') {
+        clearInfo();
+        showWarning('Carregando...');
 
-        let url= `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metric&lang=pt_br`
-       
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metric&lang=pt_br`;
+
         let results = await fetch(url);
         let json = await results.json();
-      
-        if(json.cod === 200){
+
+        if (json.cod === 200) {
             showInfo({
                 name: json.name,
                 country: json.sys.country,
                 temp: json.main.temp,
                 tempIcon: json.weather[0].icon,
                 windSpeed: json.wind.speed,
-        
-                descri : json.weather[0].description,
-
-            } 
-            );
-        }
-        else{
+                descri: json.weather[0].description,
+                humidity: json.main.humidity, // Umidade adicionada aqui
+            });
+        } else {
             clearInfo();
             showWarning('Não encontramos essa localização');
         }
-    }else{
+    } else {
         clearInfo();
     }
-})
+});
 
-
-function showInfo(json){
+function showInfo(json) {
     showWarning('');
-    document.querySelector('.resultado').style.display= 'block';
-    document.querySelector('.titulo').innerHTML= `${json.name}, ${json.country}`
+    document.querySelector('.resultado').style.display = 'block';
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
     document.querySelector('.temperatura').innerHTML = `${json.temp} <sup>ºC</sup>`;
     document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
-    document.querySelector('.tempInfo').innerHTML= `${json.descri} `;
-    document.querySelector('.informacoes img').setAttribute('src',`assets/images/${json.tempIcon}.gif`);
+    document.querySelector('.tempInfo').innerHTML = `${json.descri}`;
+    document.querySelector('.informacoes img').setAttribute('src', `assets/images/${json.tempIcon}.gif`);
 
-
+    // Exibir a umidade
+    document.querySelector('.umidadeInfo').innerHTML = `${json.humidity} <span>%</span>`;
 }
 
-function showWarning(msg)
-{
-    document.querySelector('.aviso').innerHTML = msg
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
 }
 
 function clearInfo() {
@@ -59,39 +52,34 @@ function clearInfo() {
     document.querySelector('.resultado').style.display = 'none';
 }
 
- async function petrolina (){
-    let input = 'petrolina'
-    if(input !=='')
-    {
-        clearInfo();    
-        showWarning('Carregando...')
-        
+async function aracaju() {
+    let input = 'Aracaju';
+    if (input !== '') {
+        clearInfo();
+        showWarning('Carregando...');
 
-        let url= `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metric&lang=pt_br`
-       
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metric&lang=pt_br`;
+
         let results = await fetch(url);
         let json = await results.json();
-      
-        if(json.cod === 200){
+
+        if (json.cod === 200) {
             showInfo({
                 name: json.name,
                 country: json.sys.country,
                 temp: json.main.temp,
                 tempIcon: json.weather[0].icon,
                 windSpeed: json.wind.speed,
-        
-                descri : json.weather[0].description,
-
-            } 
-            );
-        }
-        else{
+                descri: json.weather[0].description,
+                humidity: json.main.humidity, // Umidade adicionada aqui
+            });
+        } else {
             clearInfo();
             showWarning('Não encontramos essa localização');
         }
-    }else{
+    } else {
         clearInfo();
     }
 }
 
-petrolina();
+aracaju();
